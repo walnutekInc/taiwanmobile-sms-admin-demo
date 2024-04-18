55aef09c1edc9545465b797a5ab7194a9f7e9d0a
TingHuang222324 <t99635@gmail.com>
2024-04-18 12:09:42 +0800

    update static pages

--- assets/contact-person-ea4eecf2.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./contact-person.vuevuetypescriptsetuptruelang-0ebcb5ad.js";import"./contact-person-2938018d.js";import"./antd-b38f92bc.js";import"./vue-7f4bbefd.js";import"./index-acc9caf3.js";import"./GenderEnum-9570089c.js";import"./index.vuevuetypescriptsetuptruelang-df83ccd5.js";export{o as default};
--- assets/redirect-64cbbc38.js
+++
@@ -1,1 +0,0 @@
-import{d as a,W as c,ac as n,ad as r,V as p,X as d}from"./vue-7f4bbefd.js";const u=d("h1",null,"Redirecting...",-1),_=[u],f=a({__name:"redirect",setup(l){const s=n(),t=r(),e=s.params,o=e!=null&&e.path?decodeURIComponent(e.path):"";return o?t.replace(o):t.replace("/"),(i,h)=>(p(),c("div",null,_))}});export{f as default};
