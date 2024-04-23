bf33d7eb451bc7abbebe57658128e5edca9b3f74
TingHuang222324 <t99635@gmail.com>
2024-04-23 15:17:08 +0800

    update static page

---
+++ assets/contact-person-b2bab001.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./contact-person.vuevuetypescriptsetuptruelang-7a12fd82.js";import"./request-6ddf16f6.js";import"./antd-b020a38c.js";import"./vue-b9a22f97.js";import"./index-dfeff907.js";import"./GenderEnum-64fc5a45.js";import"./index-0ae3ed2a.js";import"./index.vuevuetypescriptsetuptruelang-4fca776e.js";import"./tools-a2ffc173.js";export{o as default};
