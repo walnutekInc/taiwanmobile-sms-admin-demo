67faa62b3771b05ea99a5f54877f10607714f38c
TingHuang222324 <t99635@gmail.com>
2024-05-24 11:45:33 +0800

    update page

--- assets/redirect-C3xavfhq.js
+++
@@ -1,1 +0,0 @@
-import{d as a,X as c,ac as n,ad as r,W as p,Y as d}from"./vue-B4jB9Tcu.js";const u=d("h1",null,"Redirecting...",-1),_=[u],f=a({__name:"redirect",setup(l){const s=n(),t=r(),e=s.params,o=e!=null&&e.path?decodeURIComponent(e.path):"";return o?t.replace(o):t.replace("/"),(i,h)=>(p(),c("div",null,_))}});export{f as default};
