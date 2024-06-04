55e281b59290668f5004baa1435db307ab31c52f
TingHuang222324 <t99635@gmail.com>
2024-06-04 14:16:22 +0800

    update page

--- assets/index.vuevuetypescriptsetuptruelang-DW3HDLpP.js
+++
@@ -1,1 +0,0 @@
-import{d as o,l as r,W as s,_ as t,u as c}from"./vue-Cot4Dobd.js";const d=o({__name:"index",props:{vnodes:{type:[Object,Function],required:!0}},setup(e){const n=r(e.vnodes);return(a,u)=>(s(),t(c(n)))}});export{d as _};
